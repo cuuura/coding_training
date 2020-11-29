ddf36368603294fcd08ebf15cbd44ca918098a77
Js.Park <curios.cura@gmail.com>
2020-11-29 16:44:25 +0900

    Java ex01 풀이

---
+++ ex01/Java/Ex01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Ex01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String myName = scanner.next();
+
+        // String format 
+        System.out.println(String.format("Hello, %s, nice to meet you!", myName));
+        // System.out.printf("Hello, %s, nice to meet you!", myName);
+        // System.out.println("Hello, " + myName + ", nice to meet you!");
+    }
+}
